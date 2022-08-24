b297a64e2a7d1f00ac5c2a2a4c29e9ea893779aa
Ramana Kumar <ramana@member.fsf.org>
2022-08-24 22:16:09 +0000

    Add support for cbETH

--- lsd.js
+++ lsd.js
@@ -15,10 +15,13 @@ const ramanaAddress = '0xb0de8cb8dcc8c5382c4b7f3e978b491140b2bc55';
 const truncatedAddress = `${ramanaAddress.substring(0,6)}…${ramanaAddress.substring(ramanaAddress.length - 4)}`
 const rETHAddress = '0xae78736Cd615f374D3085123A210448E74Fc6393';
 const wstETHAddress = '0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0';
+const cbETHAddress = '0xbe9895146f7af43049ca1c1ae358b0541ea49704';
 const rETHContract = new ethers.Contract(rETHAddress,
   ['function getExchangeRate() view returns (uint256)'], provider);
 const wstETHContract = new ethers.Contract(wstETHAddress,
   ['function stEthPerToken() view returns (uint256)'], provider);
+const cbETHContract = new ethers.Contract(cbETHAddress,
+  ['function exchangeRate() view returns (uint256)'], provider);
 const spotPriceContract = new ethers.Contract('0x07D91f5fb9Bf7798734C3f606dB065549F6893bb',
   ['function getRateToEth(address, bool) view returns (uint256)'], provider);
 
@@ -45,17 +48,22 @@ app.post('/', verifyKeyMiddleware(process.env.PUBLIC_KEY), (req, res) => {
     Promise.all(
       [rETHContract.getExchangeRate(),
         wstETHContract.stEthPerToken(),
+        cbETHContract.exchangeRate(),
         secondaryRate(rETHAddress),
-        secondaryRate(wstETHAddress)]).then(prices => {
-          const rETH = percentage(prices[0], prices[2], rETHAddress)
-          const wstETH = percentage(prices[1], prices[3], wstETHAddress)
+        secondaryRate(wstETHAddress),
+        secondaryRate(cbETHAddress)]).then(prices => {
+          const rETH = percentage(prices[0], prices[3], rETHAddress)
+          const wstETH = percentage(prices[1], prices[4], wstETHAddress)
+          const cbETH = percentage(prices[2], prices[5], cbETHAddress)
           const lines = [
             '_Primary_',
             `**[1 rETH = ${rateToString(prices[0])} ETH](https://stake.rocketpool.net)**`,
             `**[1 wstETH = ${rateToString(prices[1])} ETH](https://stake.lido.fi/wrap)**`,
+            `**[1 cbETH = ${rateToString(prices[2])} ETH](https://www.coinbase.com/cbeth/whitepaper)**`,
             `_Secondary ([1Inch](https://app.1inch.io/#/r/${ramanaAddress}))_`,
-            `**[1 rETH = ${rateToString(prices[2])} ETH](https://app.1inch.io/#/1/classic/limit-order/${rETH.u})** (${rETH.p}% ${rETH.d})`,
-            `**[1 wstETH = ${rateToString(prices[3])} ETH](https://app.1inch.io/#/1/classic/limit-order/${wstETH.u})** (${wstETH.p}% ${wstETH.d})`,
+            `**[1 rETH = ${rateToString(prices[3])} ETH](https://app.1inch.io/#/1/classic/limit-order/${rETH.u})** (${rETH.p}% ${rETH.d})`,
+            `**[1 wstETH = ${rateToString(prices[4])} ETH](https://app.1inch.io/#/1/classic/limit-order/${wstETH.u})** (${wstETH.p}% ${wstETH.d})`,
+            `**[1 cbETH = ${rateToString(prices[5])} ETH](https://app.1inch.io/#/1/classic/limit-order/${cbETH.u})** (${cbETH.p}% ${cbETH.d})`,
             `_[bot](https://github.com/xrchz/discord) by ramana.eth (${truncatedAddress})_`,
           ]
           res.send({
