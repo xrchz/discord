6001922e7e0e577583e6f20a052e3464c32497d1
Ramana Kumar <ramana@member.fsf.org>
2023-08-12 12:40:12 +0000

    Update for new 1inch permissioned api

--- lsd.js
+++ lsd.js
@@ -7,6 +7,9 @@ import { InteractionType, InteractionResponseType, verifyKeyMiddleware } from 'd
 
 const app = express();
 
+const discordApi = 'https://discord.com/api/v10'
+const userAgentVersion = 1
+
 const provider = new ethers.EtherscanProvider('mainnet', process.env.ETHERSCAN_KEY);
 const ramanaAddressOneInch = '0xB0De8cB8Dcc8c5382c4b7F3E978b491140B2bC55';
 const ramanaAddress = '0x65FE89a480bdB998F4116DAf2A9360632554092c';
@@ -37,30 +40,43 @@ const rateToString = r => {
   return ethers.formatUnits(r - rem)
 }
 
+const waitBetweenCalls = 1500
+const callQueue = []
+async function rateLimit(getCall) {
+  const waitTurn = new Promise(resolve => callQueue.push(resolve))
+  if (callQueue.length) callQueue[0]()
+  await waitTurn.then(() => callQueue.shift())
+  await new Promise(resolve => setTimeout(resolve, waitBetweenCalls))
+  if (callQueue.length) callQueue[0]()
+  return await getCall()
+}
+
 async function secondaryRate(addr) {
   const quoteParams = {
-    fromTokenAddress: addr,
-    toTokenAddress: ethAddress,
+    src: addr,
+    dst: ethAddress,
     amount: oneEtherStr
   }
   const queryString = new URLSearchParams(quoteParams).toString()
-  const url = `https://api.1inch.io/v5.0/1/quote?${queryString}`
-  const apiCall = new Promise((resolve, reject) => {
-    const req = https.get(url,
+  const url = `https://api.1inch.dev/swap/v5.2/1/quote?${queryString}`
+  const apiCall = () => new Promise((resolve, reject) => {
+    const req = https.get(url, {headers: {'Authorization': `Bearer ${process.env.API_KEY}`}},
       (res) => {
         if (res.statusCode !== 200) {
           console.log(`Got ${res.statusCode} from 1inch: ${res.statusMessage}`)
           reject(res)
         }
-        res.setEncoding('utf8')
-        let data = ''
-        res.on('data', (chunk) => data += chunk)
-        res.on('end', () => resolve(JSON.parse(data)))
+        else {
+          res.setEncoding('utf8')
+          let data = ''
+          res.on('data', (chunk) => data += chunk)
+          res.on('end', () => resolve(JSON.parse(data)))
+        }
       })
     req.on('error', reject)
   })
-  const quote = await apiCall
-  return BigInt(quote.toTokenAmount)
+  const quote = await rateLimit(apiCall)
+  return BigInt(quote.toAmount)
 }
 
 const abs = (n) => n < 0n ? -n : n
@@ -77,42 +93,66 @@ const percentage = (p, s, addr) => {
   }
 }
 
+const followupOptions = {
+  headers: {
+    'Authorization': `Bearer ${process.env.TOKEN}`,
+    'User-Agent': `DiscordBot (https://xrchz.net/lsd-price, ${userAgentVersion})`,
+    'Content-Type': 'application/json'
+  },
+  method: 'PATCH'
+}
+
 app.post('/', verifyKeyMiddleware(process.env.PUBLIC_KEY), (req, res) => {
   const interaction = req.body;
+  const application_id = interaction.application_id
+  const interaction_token = interaction.token
+  const followupUrl = new URL(`${discordApi}/webhooks/${application_id}/${interaction_token}/messages/@original`)
+  const suppress_embeds = true
+  const ephemeral = interaction.guild_id === rocketPoolId && !broadcastChannelIds.includes(interaction.channel_id)
+  const sendFollowup = msg => {
+    const followupReq = https.request(followupUrl, followupOptions, followupRes => {
+      if (followupRes.statusCode !== 200) {
+        console.log(`Got ${followupRes.statusCode} from Discord: ${followupRes.statusMessage}`)
+      }
+    })
+    followupReq.end(JSON.stringify({content: msg}))
+  }
   if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+    res.send({
+      type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
+      content: 'Waiting for 1Inch...',
+      flags: suppress_embeds<<2 | ephemeral<<6
+    })
     Promise.all(
       [rETHContract.getExchangeRate(),
-       wstETHContract.stEthPerToken(),
-       cbETHContract.exchangeRate(),
-       secondaryRate(rETHAddress),
-       secondaryRate(wstETHAddress),
-       secondaryRate(cbETHAddress)
+        wstETHContract.stEthPerToken(),
+        cbETHContract.exchangeRate(),
+        secondaryRate(rETHAddress),
+        secondaryRate(wstETHAddress),
+        secondaryRate(cbETHAddress)
       ]).then(prices => {
-          const rETH = percentage(prices[0], prices[3], rETHAddress)
-          const wstETH = percentage(prices[1], prices[4], wstETHAddress)
-          const cbETH = percentage(prices[2], prices[5], cbETHAddress)
-          const lines = [
-            '_Primary_',
-            `**[1 rETH = ${rateToString(prices[0])} ETH](https://stake.rocketpool.net)**`,
-            `**[1 wstETH = ${rateToString(prices[1])} ETH](https://stake.lido.fi/wrap)**`,
-            `**[1 cbETH = ${rateToString(prices[2])} ETH](https://www.coinbase.com/cbeth/whitepaper)**`,
-            // 'Warning: these are liquidity-weighted average prices, not best prices. Will switch to best at some point.',
-            `_Secondary ([1Inch](https://app.1inch.io/#/r/${ramanaAddressOneInch}))_`,
-            `**[1 rETH = ${rateToString(prices[3])} ETH](https://app.1inch.io/#/1/classic/limit-order/${rETH.u})** (${rETH.p}% ${rETH.d})`,
-            `**[1 wstETH = ${rateToString(prices[4])} ETH](https://app.1inch.io/#/1/classic/limit-order/${wstETH.u})** (${wstETH.p}% ${wstETH.d})`,
-            `**[1 cbETH = ${rateToString(prices[5])} ETH](https://app.1inch.io/#/1/classic/limit-order/${cbETH.u})** (${cbETH.p}% ${cbETH.d})`,
-            `_[bot](https://github.com/xrchz/discord) by ramana.eth (${truncatedAddress})_`,
-          ]
-          const suppress_embeds = true
-          const ephemeral = interaction.guild_id === rocketPoolId && !broadcastChannelIds.includes(interaction.channel_id)
-          res.send({
-            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            data: {
-              content: lines.join('\n'),
-              flags: suppress_embeds<<2 | ephemeral<<6,
-            },
-          });
-        })
+        const rETH = percentage(prices[0], prices[3], rETHAddress)
+        const wstETH = percentage(prices[1], prices[4], wstETHAddress)
+        const cbETH = percentage(prices[2], prices[5], cbETHAddress)
+        const lines = [
+          '_Primary_',
+          `**[1 rETH = ${rateToString(prices[0])} ETH](<https://stake.rocketpool.net>)**`,
+          `**[1 wstETH = ${rateToString(prices[1])} ETH](<https://stake.lido.fi/wrap>)**`,
+          `**[1 cbETH = ${rateToString(prices[2])} ETH](<https://www.coinbase.com/cbeth/whitepaper>)**`,
+          // 'Warning: these are liquidity-weighted average prices, not best prices. Will switch to best at some point.',
+          `_Secondary ([1Inch](<https://app.1inch.io/#/r/${ramanaAddressOneInch}>))_`,
+          `**[1 rETH = ${rateToString(prices[3])} ETH](<https://app.1inch.io/#/1/classic/limit-order/${rETH.u}>)** (${rETH.p}% ${rETH.d})`,
+          `**[1 wstETH = ${rateToString(prices[4])} ETH](<https://app.1inch.io/#/1/classic/limit-order/${wstETH.u}>)** (${wstETH.p}% ${wstETH.d})`,
+          `**[1 cbETH = ${rateToString(prices[5])} ETH](<https://app.1inch.io/#/1/classic/limit-order/${cbETH.u}>)** (${cbETH.p}% ${cbETH.d})`,
+          `_[bot](<https://github.com/xrchz/discord>) by ramana.eth (${truncatedAddress})_`,
+        ]
+        sendFollowup(lines.join('\n'))
+      })
+      .catch(error => {
+        const message = ('statusMessage' in error) ? error.statusMessage : JSON.stringify(error)
+        const shortMessage = message.length > 32 ? `${message.slice(0, 32)}...` : message
+        sendFollowup(shortMessage)
+      })
   }
 });
 
