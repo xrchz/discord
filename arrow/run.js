dbe59f4780bbcaf732ff0ee7c38b4a0dceddd245
Ramana Kumar <ramana@member.fsf.org>
2024-04-02 22:25:50 +0000

    Add map embed

--- arrow/run.js
+++ arrow/run.js
@@ -21,6 +21,46 @@ const followupOptions = {
   method: 'PATCH'
 }
 
+const MAX_MAPS = 12
+const maps = new Map()
+
+async function ensureMap(coords) {
+  if (maps.has(coords)) {
+    console.log(`${Date()}: map cache hit for ${coords}`)
+    return
+  }
+  console.log(`${Date()}: fetching map for ${coords}`)
+  const res = await fetch(`https://maps.googleapis.com/maps/api/staticmap?key=${process.env.MAPS_API_KEY}&size=256x256&maptype=hybrid&markers=label:ROSARIA|${coords}`)
+  if (res.status !== 200) {
+    const error = `${res.status}: ${await res.text()}`
+    console.warn(`${Date()}: Maps returned ${error}`)
+    throw {statusMessage: error}
+  }
+  const buffer = Buffer.from(await res.arrayBuffer())
+  console.log(`Got map of ${buffer.length} bytes for ${coords}`)
+  maps.set(coords, buffer)
+  if (maps.size > MAX_MAPS) {
+    const key = maps.keys().next().value
+    console.log(`${Date()}: deleting ${key} from cache`)
+    maps.delete(key)
+  }
+}
+
+app.get('/map/:coords([-+0-9.,]{3,}).png', (req, res) => {
+  if (!maps.has(req.params.coords)) {
+    console.warn(`${Date()}: map cache miss for ${req.params.coords}`)
+    return res.status(404).end()
+  }
+  const buffer = maps.get(req.params.coords)
+  res.type('png')
+  res.send(buffer)
+})
+
+app.get('*', (req, res, next) => {
+  console.warn(`${Date()}: unexpected GET path ${req.path}`)
+  next()
+})
+
 app.post('/', verifyKeyMiddleware(process.env.PUBLIC_KEY), (req, res) => {
   const interaction = req.body;
   const application_id = interaction.application_id
@@ -28,13 +68,13 @@ app.post('/', verifyKeyMiddleware(process.env.PUBLIC_KEY), (req, res) => {
   const followupUrl = new URL(`${discordApi}/webhooks/${application_id}/${interaction_token}/messages/@original`)
   const suppress_embeds = true
   const ephemeral = interaction.guild_id === arrowServerId && nonBroadcastChannelIds.includes(interaction.channel_id)
-  const sendFollowup = msg => {
+  const sendFollowup = ({content, embeds}) => {
     const followupReq = https.request(followupUrl, followupOptions, followupRes => {
       if (followupRes.statusCode !== 200) {
         console.log(`Got ${followupRes.statusCode} from Discord: ${followupRes.statusMessage}`)
       }
     })
-    followupReq.end(JSON.stringify({content: msg}))
+    followupReq.end(JSON.stringify({content, embeds}))
   }
   if (interaction.type === InteractionType.APPLICATION_COMMAND) {
     res.send({
@@ -50,18 +90,23 @@ app.post('/', verifyKeyMiddleware(process.env.PUBLIC_KEY), (req, res) => {
       return res.json().then(data => {
         const {AIS} = data[0]
         const etastamp = Math.round(Date.parse(`${AIS.ETA.replace(' ', 'T')}Z`) / 1000)
-        const lines = [
-          `Vessel ${AIS.IMO}: ${AIS.NAME} (${AIS.CALLSIGN})`,
-          `Current Position: ${AIS.LATITUDE}, ${AIS.LONGITUDE}`,
-          `Estimated to reach ${AIS.DESTINATION}: ${AIS.ETA} (<t:${etastamp}:R>)`
-        ]
-        sendFollowup(lines.join('\n'))
+        const {LATITUDE, LONGITUDE} = AIS
+        const coords = `${LATITUDE},${LONGITUDE}`
+        return ensureMap(coords).then(() => {
+          const lines = [
+            `Vessel ${AIS.IMO}: ${AIS.NAME} (${AIS.CALLSIGN})`,
+            `Current Position: ${LATITUDE}, ${LONGITUDE}`,
+            `Speed: ${AIS.SPEED} knots`,
+            `Estimated to reach ${AIS.DESTINATION}: ${AIS.ETA} (<t:${etastamp}:R>)`
+          ]
+          sendFollowup({content: lines.join('\n'), embeds: [{image:{url:`https://xrchz.net/arrowbot/map/${coords}.png`}}]})
+        })
       })
     }).catch(error => {
       const message = ('statusMessage' in error) ? error.statusMessage : JSON.stringify(error)
       console.warn(`${Date()}: error ${error}:${typeof error} serving response: ${message}`)
       const shortMessage = message.length > 32 ? `${message.slice(0, 32)}...` : message
-      sendFollowup(shortMessage)
+      sendFollowup({content: shortMessage})
     })
   }
 });
